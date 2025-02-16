568cf767642a3775f3b32a8e2540ae9cee4fd999
BigBen131283 <benjamin.toubhans@orange.fr>
2025-02-16 08:31:47 +0100

    250216

--- 1. Fichiers de départ/37.Occurences/script.js
+++ 1. Fichiers de départ/37.Occurences/script.js
@@ -1,6 +1,12 @@
 function occurrences(str, letter){
-
+  return str.split("").filter(el => el === letter).length
 }
+
+function occurrencesCorrection(str, letter) {
+  return str.split(letter).length - 1
+}
+
+const occurrencesOneLinerCorrection = (str, letter) => str.split(letter).length - 1
 
 /* ÉNONCÉ 📚 */
 
@@ -14,3 +20,13 @@ console.log(occurrences("lhelol", "l")); // 3
 console.log(occurrences("abcde", "e")); // 1
 console.log(occurrences("zzzzzzzzzzzzz", "z")); // 13
 console.log(occurrences("La victoire est à nous", "e")); // 2
+
+console.log(occurrencesCorrection("lhelol", "l")); // 3
+console.log(occurrencesCorrection("abcde", "e")); // 1
+console.log(occurrencesCorrection("zzzzzzzzzzzzz", "z")); // 13
+console.log(occurrencesCorrection("La victoire est à nous", "e")); // 2
+
+console.log(occurrencesOneLinerCorrection("lhelol", "l")); // 3
+console.log(occurrencesOneLinerCorrection("abcde", "e")); // 1
+console.log(occurrencesOneLinerCorrection("zzzzzzzzzzzzz", "z")); // 13
+console.log(occurrencesOneLinerCorrection("La victoire est à nous", "e")); // 2
